fefd87117857a0b6e19dcf7e13a9850e301b8b30
mpcarolin <mpcarolin@gmail.com>
2018-07-04 13:27:03 -0700

    refactoring

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -7,9 +7,16 @@ import pokeball from "../../assets/icons/pokeball/poke.png"
 import hm from "../../assets/icons/hm/fighting.png"
 import unknown from "../../assets/icons/pokemon/regular/unown.png"
 
+const pages = [
+  { name: "Pokémon", icon: pikachu, route: "/pokemon" },
+  { name: "Moves", icon:  hm, route: "/moves" },
+  { name: "Items", icon: pokeball, route: "/items" },
+  { name: "About", icon: unknown, route: "/about" } 
+]
+
 const NavLinks = (props) => {
   return (
-    <ul className="nav-link-desktop-container">
+    <ul className={ props.containerClass || "" }>
       { 
         props.links.map(link => <span className="nav-link-flex-container" key={ link.name }>
                                   <img width="50px" className="nav-link-icon" src={link.icon} />
@@ -20,53 +27,18 @@ const NavLinks = (props) => {
   ) 
 }
 
-class SimpleButton extends Component {
-  state = { clicked: false }
-
-  handleClick = () => {
-    this.setState(prev => ({ clicked: !prev.clicked }))
-    if (this.props.handleClick) {
-      this.props.handleClick()
-    }
-  }
-
-  getClass = () => {
-     return "sidebar-btn fas fa-bars "
-  }
-
-  render () {
-    return (
-      <button className={ this.getClass() }
-              onClick={ this.handleClick } />
-    ) 
-  }
-}
-
 const Sidebar = (props) => {
   if (!props.visible) return <i style={{display: "none"}}/>
   return (
     <div className="sidebar"> 
         <i onClick={ props.handleClick } className="nav-icon far fa-times-circle"></i>
-        <ul className="sidebar-ul">
-          { 
-            props.links.map(link => <span className='nav-link-flex-container' key={ link.name }>
-                                      <img width="50px" className="nav-link-icon" src={link.icon} />
-                                      <li className='nav-link'>{ link.name }</li>
-                                    </span>)
-          }
-      </ul>
+        <NavLinks links={props.links} containerClass="sidebar-ul" />
     </div>
   )  
 }
 
 class Welcome extends Component {
   state = {
-    links: [
-      { name: "Pokémon", icon: pikachu },
-      { name: "Moves", icon:  hm },
-      { name: "Items", icon: pokeball },
-      { name: "About", icon: unknown }
-    ],
     showSidebar: false,
   }
 
@@ -80,14 +52,14 @@ class Welcome extends Component {
     return (
       <div className="welcome-root">
         <div className="nav-bar">
-          <SimpleButton handleClick={this.handleClick} />
+          <button className="sidebar-btn fas fa-bars" onClick={ this.handleClick } />
           <img className="vertical-align site-logo" src={logo} height="50px" />
           <h1 className="nav-title">My Pokédex!</h1>
-          <NavLinks links={this.state.links} />
+          <NavLinks links={pages} containerClass="nav-link-desktop-container" />
         </div>
         <Sidebar handleClick={this.handleClick}
                  visible={this.state.showSidebar} 
-                 links={this.state.links} />
+                 links={pages} />
       </div>
     );
   }
