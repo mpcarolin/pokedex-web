faaa5fb49156cd3a6c1f0d2563ba8e65805fb3a9
mpcarolin <mpcarolin@gmail.com>
2018-07-05 08:34:57 -0700

    started work on filters. still some bugs

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -86,35 +86,66 @@ const CardList = (props) => {
 }
 
 const FilterBar = (props) => {
+  const placeholder = <option key="" value="" disabled>Type</option>
+  const defaultType = <option key="any" value="any">Any</option>
   return (
     <div className="filter-bar-container">
       <i style={{color: "var(--pokedex-grey)"}}className="fas fa-search" />
       <input className="filter-bar" 
              placeholder="Filter Pokemon..." 
-             onChange={props.onChange} />
+             onChange={ (e) => props.onChange({text: e.target.value}) } />
+      <select defaultValue="" className="type-select" onChange={ (e) => props.onChange({type: e.target.value})} >
+      {
+        [placeholder, defaultType].concat(Object.values(Types).map(type =>
+                  <option key={ type } value={ type }>
+                    { type.charAt(0).toUpperCase() + type.slice(1) }
+                  </option>
+               ))
+      }
+      </select>
     </div>
   )
+}
+
+
+const matches = (p, text, type) => {
+    let visible = true
+    if (text && text.length > 0) {
+      visible = p.name.toLowerCase().includes(text.toLowerCase())
+    }
+
+    if (type && (type !== "any")) {
+      visible = p.types.includes(type)
+    }
+    return visible
+}
+
+const notEmpty = (s) => {
+  return s && (s.length > 0)
 }
 
 class Pokemon extends Component {
   state = {
     filterText: "",
+    filterType: "any",
     pokemon: pokemon
   }
 
-  updateText = (e) => {
-    const newText = e.target.value
+  updateFilter = ({text, type}) => {
+    let newText = notEmpty(text) ? text : ""
+    let newType = notEmpty(type) ? type : "any"
+    console.log(type, newType)
     this.setState(prev => ({
       filterText: newText,
-      pokemon: pokemon.filter(p => ((newText === "") || p.name.toLowerCase().includes(newText.toLowerCase())))
+      filterType: newType,
+      pokemon: pokemon.filter(poke => matches(poke, newText, newType))
     }))
   }
-
 
   render() {
     return (
       <div className="pokemon-root">
-        <FilterBar onChange={this.updateText}/>
+        <FilterBar onChange={this.updateFilter}/>
         <CardList elements={this.state.pokemon} />
       </div>
     )
